e55d46a37ed3507e28d5be978ab66092ef86ed42
Alex Walker <walkernalex@gmail.com>
2025-07-20 14:30:34 -0400

    Fix: script loading bug by correctly adding defer attribute to script tag

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // Define HTML elements
 const board = document.getElementById('game-board');
 
+console.log(board);
+/*
 // Define game variables
 let snake = [{x: 10, y: 10}];
 
 // Draw game map, snake, food
 function draw() {
-    board.innerHTML = '';
+    board.innerHTML = ' '; // Clear the board
     drawSnake ();
-     drawFood ();
 }
 
 // Draw snake
-
 function drawSnake () {
 snake.forEach((segment) => {
         const snakeElement = createGameElement('div', 'snake');
         setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
 });
 }
 
@@ -32,3 +33,7 @@ function setPosition(element, position) {
     element.style.gridColumn = position.x;
     element.style.gridRow = position.y;
 }
+
+//Testing draw function 
+draw();
+*/
