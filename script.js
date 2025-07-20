bbef7f259314cb44249f760a19fa0dc35131174f
Alex Walker <walkernalex@gmail.com>
2025-07-20 14:07:36 -0400

    feat: define game variables and game logic of snake and food

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log ("test");
+// Define HTML elements
+const board = document.getElementById('game-board');
+
+// Define game variables
+let snake = [{x: 10, y: 10}];
+
+// Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake ();
+     drawFood ();
+}
+
+// Draw snake
+
+function drawSnake () {
+snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake');
+});
+}
